2b0860d98fc21afd44fadac578ef3e70e75d6d3b
xeinebiu <xeinebiu@gmail.com>
2024-10-06 12:08:20 +0200

    feat(dependy): comment out the dependy_test.dart

--- packages/dependy/test/dependy_test.dart
+++ packages/dependy/test/dependy_test.dart
@@ -1,16 +1,16 @@
-import 'package:dependy/dependy.dart';
-import 'package:test/test.dart';
-
-void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
-
-    setUp(() {
-      // Additional setup goes here.
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
-  });
-}
+// import 'package:dependy/dependy.dart';
+// import 'package:test/test.dart';
+//
+// void main() {
+//   group('A group of tests', () {
+//     final awesome = Awesome();
+//
+//     setUp(() {
+//       // Additional setup goes here.
+//     });
+//
+//     test('First Test', () {
+//       expect(awesome.isAwesome, isTrue);
+//     });
+//   });
+// }
